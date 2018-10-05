872a70a23f1db7685ff97dd82356d32419c61a2d
zemenuyar <zemenuyar@gmail.com>
2018-10-05 15:39:08 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+            int res = 0;
+            for ( int i = 1; i <= 10; i++)
+                if (i % 5 == 0 & i % 7==0)
+                    res++;
+            System.out.println();
+
+    }
+}
+
+
